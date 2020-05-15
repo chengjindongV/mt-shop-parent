4f431d0f16db6c27370a1591cc1a3d629866c62a
chengjindong <guilinsister@163.com>
2020-05-15 15:40:59 +0800

    扫描注解

--- mt-shop-service-impl/mt-shop-service-weixin/src/main/java/com/mayikt/weixin/AppWeiXin.java
+++ mt-shop-service-impl/mt-shop-service-weixin/src/main/java/com/mayikt/weixin/AppWeiXin.java
@@ -1,6 +1,7 @@
 package com.mayikt.weixin;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @EnableSwagger2Doc
+@MapperScan("com.mayikt.weixin.impl.mapper")
 public class AppWeiXin {
     public static void main(String[] args) {
         SpringApplication.run(AppWeiXin.class);
